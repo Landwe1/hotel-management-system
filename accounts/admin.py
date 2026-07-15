2df461a18ccf10c0a641ce6b73f97452fa8ef68e
Landwe Kanyanta <landwekanyanta96@gmail.com>
2026-07-15 14:03:47 -0700

    fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,23 +1,42 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+
+# Create custom forms that use phone_number instead of username
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('phone_number',)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = '__all__'
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm  # <--- This uses the correct creation form!
+
     list_display = ['phone_number', 'email', 'role', 'is_staff', 'is_active']
     list_filter = ['role', 'is_staff', 'is_active']
+    
     fieldsets = (
         (None, {'fields': ('phone_number', 'password')}),
         ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'role')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
+    
+    # We let Django handle the password setup safely here
     add_fieldsets = (
         (None, {
-            'classes': ('toggle',),
-            'fields': ('phone_number', 'password', 'role', 'is_staff', 'is_active'),
+            'classes': ('wide',),
+            'fields': ('phone_number', 'role', 'is_staff', 'is_active'), 
         }),
     )
+    
     search_fields = ['phone_number', 'email']
     ordering = ['phone_number']
 
